556d1924b5bf38d1d03014a46fe76d5ee3250ddc
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-03-16 11:59:32 -0500

    feat: add func for pointer to string slice

--- vela/vela.go
+++ vela/vela.go
@@ -4,22 +4,26 @@
 
 package vela
 
-// Bool is a helper routine that allocates a new bool value
-// to store v and returns a pointer to it.
+// Bool is a helper routine that allocates a new boolean
+// value to store v and returns a pointer to it.
 func Bool(v bool) *bool { return &v }
 
-// Int is a helper routine that allocates a new int value
-// to store v and returns a pointer to it.
+// Bytes is a helper routine that allocates a new byte
+// array value to store v and returns a pointer to it.
+func Bytes(v []byte) *[]byte { return &v }
+
+// Int is a helper routine that allocates a new integer
+// value to store v and returns a pointer to it.
 func Int(v int) *int { return &v }
 
-// Int64 is a helper routine that allocates a new int64 value
-// to store v and returns a pointer to it.
+// Int64 is a helper routine that allocates a new 64 bit
+// integer value to store v and returns a pointer to it.
 func Int64(v int64) *int64 { return &v }
 
-// String is a helper routine that allocates a new string value
-// to store v and returns a pointer to it.
+// String is a helper routine that allocates a new string
+// value to store v and returns a pointer to it.
 func String(v string) *string { return &v }
 
-// Bytes is a helper routine that allocates a new byte array value
-// to store v and returns a pointer to it.
-func Bytes(v []byte) *[]byte { return &v }
+// Strings is a helper routine that allocates a new string
+// array value to store v and returns a pointer to it.
+func Strings(v []string) *[]string { return &v }
---
+++ vela/vela_test.go
@@ -0,0 +1,94 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package vela
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestVela_Bool(t *testing.T) {
+	// setup types
+	_bool := false
+
+	want := &_bool
+
+	// run test
+	got := Bool(_bool)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Bool is %v, want %v", got, want)
+	}
+}
+
+func TestVela_Bytes(t *testing.T) {
+	// setup types
+	_bytes := []byte("foo")
+
+	want := &_bytes
+
+	// run test
+	got := Bytes(_bytes)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Bytes is %v, want %v", got, want)
+	}
+}
+
+func TestVela_Int(t *testing.T) {
+	// setup types
+	_int := 1
+
+	want := &_int
+
+	// run test
+	got := Int(_int)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Int is %v, want %v", got, want)
+	}
+}
+
+func TestVela_Int64(t *testing.T) {
+	// setup types
+	_int64 := int64(1)
+
+	want := &_int64
+
+	// run test
+	got := Int64(_int64)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Int64 is %v, want %v", got, want)
+	}
+}
+
+func TestVela_String(t *testing.T) {
+	// setup types
+	_string := "foo"
+
+	want := &_string
+
+	// run test
+	got := String(_string)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("String is %v, want %v", got, want)
+	}
+}
+
+func TestVela_Strings(t *testing.T) {
+	// setup types
+	_strings := []string{"foo"}
+
+	want := &_strings
+
+	// run test
+	got := Strings(_strings)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Strings is %v, want %v", got, want)
+	}
+}
