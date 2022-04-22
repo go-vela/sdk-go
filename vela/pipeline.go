953760d4f67c09f340a24cd3a1dcff25f040f907
JordanBrockopp <Jordan_Brockopp@cargill.com>
2022-04-22 13:03:07 -0500

    fix(pipeline): support for API changes v2

--- vela/pipeline.go
+++ vela/pipeline.go
@@ -6,6 +6,7 @@ package vela
 
 import (
 	"fmt"
+	"github.com/go-vela/types/library"
 
 	"github.com/go-vela/types/yaml"
 )
@@ -33,7 +34,7 @@ type PipelineOptions struct {
 }
 
 // Get returns the provided pipeline.
-func (svc *PipelineService) Get(org, repo, ref string, opt *PipelineOptions) (*yaml.Build, *Response, error) {
+func (svc *PipelineService) Get(org, repo, ref string, opt *PipelineOptions) (*library.Pipeline, *Response, error) {
 	// set the API endpoint path we send the request to
 	u := fmt.Sprintf("/api/v1/pipelines/%s/%s/%s", org, repo, ref)
 
@@ -43,11 +44,73 @@ func (svc *PipelineService) Get(org, repo, ref string, opt *PipelineOptions) (*y
 		return nil, nil, err
 	}
 
-	// yaml Build type we want to return
-	v := new(yaml.Build)
+	// library Pipeline type we want to return
+	v := new(library.Pipeline)
 
 	// send request using client
 	resp, err := svc.client.Call("GET", u, nil, v)
+
+	return v, resp, err
+}
+
+// GetAll returns a list of all pipelines.
+func (svc *PipelineService) GetAll(org, repo string, opt *PipelineOptions) (*[]library.Pipeline, *Response, error) {
+	// set the API endpoint path we send the request to
+	u := fmt.Sprintf("/api/v1/pipelines/%s/%s", org, repo)
+
+	// add optional arguments if supplied
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// slice library Pipeline type we want to return
+	v := new([]library.Pipeline)
+
+	// send request using client
+	resp, err := svc.client.Call("GET", u, nil, v)
+
+	return v, resp, err
+}
+
+// Add constructs a pipeline with the provided details.
+func (svc *PipelineService) Add(org, repo string, h *library.Pipeline) (*library.Pipeline, *Response, error) {
+	// set the API endpoint path we send the request to
+	u := fmt.Sprintf("/api/v1/pipelines/%s/%s", org, repo)
+
+	// library Pipeline type we want to return
+	v := new(library.Pipeline)
+
+	// send request using client
+	resp, err := svc.client.Call("POST", u, h, v)
+
+	return v, resp, err
+}
+
+// Update modifies a pipeline with the provided details.
+func (svc *PipelineService) Update(org, repo string, p *library.Pipeline) (*library.Pipeline, *Response, error) {
+	// set the API endpoint path we send the request to
+	u := fmt.Sprintf("/api/v1/pipelines/%s/%s/%s", org, repo, p.GetCommit())
+
+	// library Pipeline type we want to return
+	v := new(library.Pipeline)
+
+	// send request using client
+	resp, err := svc.client.Call("PUT", u, p, v)
+
+	return v, resp, err
+}
+
+// Remove deletes the provided pipeline.
+func (svc *PipelineService) Remove(org, repo string, pipeline string) (*string, *Response, error) {
+	// set the API endpoint path we send the request to
+	u := fmt.Sprintf("/api/v1/pipelines/%s/%s/%s", org, repo, pipeline)
+
+	// string type we want to return
+	v := new(string)
+
+	// send request using client
+	resp, err := svc.client.Call("DELETE", u, nil, v)
 
 	return v, resp, err
 }
