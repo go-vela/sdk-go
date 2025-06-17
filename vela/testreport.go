b30a945b29ae838a42d7d670e22665c39f00b74a
Kelly Merrick <kelly.merrick@target.com>
2025-06-17 10:02:46 -0500

    feat(test reports): ability to create record in db

---
+++ vela/testreport.go
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package vela
+
+import (
+	"fmt"
+
+	api "github.com/go-vela/server/api/types"
+)
+
+// TestReportService handles retrieving a test report from
+// the server methods of the Vela API.
+type TestReportService service
+
+// Add constructs a test report with the provided details.
+func (svc *TestReportService) Add(org, repo string, build int, t *api.TestReport) (*api.TestReport, *Response, error) {
+	// set the API endpoint path we send the request to
+	u := fmt.Sprintf("/api/v1/repos/%s/%s/builds/%d/reports/testreport", org, repo, build)
+
+	// API StorageInfo type we want to return
+	tr := new(api.TestReport)
+
+	// send request using client
+	resp, err := svc.client.Call("POST", u, t, tr)
+
+	return tr, resp, err
+}
