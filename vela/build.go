4cea220a14fd921cdc7f287dac688ad5a2ae25db
Tim Huynh <tim_huynh94@yahoo.com>
2026-03-02 09:26:45 -0600

    change to PUT to match server

--- vela/build.go
+++ vela/build.go
@@ -240,9 +240,9 @@ func (svc *BuildService) GetIDToken(ctx context.Context, org, repo string, build
 }
 
 func (svc *BuildService) GetPresignedPutURL(ctx context.Context, objName, org, repo string, build int64) (*api.PresignURL, *Response, error) {
-	u := fmt.Sprintf("/api/v1/repos/%s/%s/builds/%d/storage/%s/presigned-put", org, repo, build, objName)
+	u := fmt.Sprintf("/api/v1/repos/%s/%s/builds/%d/storage/%s/upload-url", org, repo, build, objName)
 	out := new(api.PresignURL)
-	resp, err := svc.client.Call(ctx, "GET", u, nil, out)
+	resp, err := svc.client.Call(ctx, "PUT", u, nil, out)
 
 	return out, resp, err
 }
